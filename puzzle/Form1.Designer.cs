9c6e3e5c605d40fac925a40be582304369121c6c
UBTUIT <UBTUIT@LENOVO>
2024-05-13 17:57:32 +0500

    Добавьте файлы проекта.

---
+++ puzzle/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmEkoPuzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEkoPuzzle));
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.gbPuzzleBox = new System.Windows.Forms.GroupBox();
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.pbx3 = new System.Windows.Forms.PictureBox();
+            this.pbx4 = new System.Windows.Forms.PictureBox();
+            this.pbx5 = new System.Windows.Forms.PictureBox();
+            this.pbx6 = new System.Windows.Forms.PictureBox();
+            this.pbx7 = new System.Windows.Forms.PictureBox();
+            this.pbx8 = new System.Windows.Forms.PictureBox();
+            this.pbx9 = new System.Windows.Forms.PictureBox();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.lblQadamlarSoni = new System.Windows.Forms.Label();
+            this.lblQolganVaqt = new System.Windows.Forms.Label();
+            this.tmrTimeElapse = new System.Windows.Forms.Timer(this.components);
+            this.btnPauza = new System.Windows.Forms.Button();
+            this.gbOriginal = new System.Windows.Forms.GroupBox();
+            this.gbPuzzleBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.Tomato;
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnQuit.ForeColor = System.Drawing.Color.White;
+            this.btnQuit.Location = new System.Drawing.Point(773, 415);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(86, 42);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Chiqish";
+            this.btnQuit.UseMnemonic = false;
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // gbPuzzleBox
+            // 
+            this.gbPuzzleBox.Controls.Add(this.pbx1);
+            this.gbPuzzleBox.Controls.Add(this.pbx2);
+            this.gbPuzzleBox.Controls.Add(this.pbx3);
+            this.gbPuzzleBox.Controls.Add(this.pbx4);
+            this.gbPuzzleBox.Controls.Add(this.pbx5);
+            this.gbPuzzleBox.Controls.Add(this.pbx6);
+            this.gbPuzzleBox.Controls.Add(this.pbx7);
+            this.gbPuzzleBox.Controls.Add(this.pbx8);
+            this.gbPuzzleBox.Controls.Add(this.pbx9);
+            this.gbPuzzleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gbPuzzleBox.Location = new System.Drawing.Point(33, 4);
+            this.gbPuzzleBox.Name = "gbPuzzleBox";
+            this.gbPuzzleBox.Size = new System.Drawing.Size(427, 433);
+            this.gbPuzzleBox.TabIndex = 0;
+            this.gbPuzzleBox.TabStop = false;
+            this.gbPuzzleBox.Text = "Boshingni qotirma!";
+            // 
+            // pbx1
+            // 
+            this.pbx1.Location = new System.Drawing.Point(17, 23);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(130, 130);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(149, 23);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(130, 130);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 0;
+            this.pbx2.TabStop = false;
+            this.pbx2.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx3
+            // 
+            this.pbx3.Location = new System.Drawing.Point(281, 23);
+            this.pbx3.Name = "pbx3";
+            this.pbx3.Size = new System.Drawing.Size(130, 130);
+            this.pbx3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx3.TabIndex = 0;
+            this.pbx3.TabStop = false;
+            this.pbx3.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx4
+            // 
+            this.pbx4.Location = new System.Drawing.Point(17, 155);
+            this.pbx4.Name = "pbx4";
+            this.pbx4.Size = new System.Drawing.Size(130, 130);
+            this.pbx4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx4.TabIndex = 0;
+            this.pbx4.TabStop = false;
+            this.pbx4.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx5
+            // 
+            this.pbx5.Location = new System.Drawing.Point(149, 155);
+            this.pbx5.Name = "pbx5";
+            this.pbx5.Size = new System.Drawing.Size(130, 130);
+            this.pbx5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx5.TabIndex = 0;
+            this.pbx5.TabStop = false;
+            this.pbx5.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx6
+            // 
+            this.pbx6.Location = new System.Drawing.Point(281, 155);
+            this.pbx6.Name = "pbx6";
+            this.pbx6.Size = new System.Drawing.Size(130, 130);
+            this.pbx6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx6.TabIndex = 0;
+            this.pbx6.TabStop = false;
+            this.pbx6.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx7
+            // 
+            this.pbx7.Location = new System.Drawing.Point(17, 287);
+            this.pbx7.Name = "pbx7";
+            this.pbx7.Size = new System.Drawing.Size(130, 130);
+            this.pbx7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx7.TabIndex = 0;
+            this.pbx7.TabStop = false;
+            this.pbx7.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx8
+            // 
+            this.pbx8.Location = new System.Drawing.Point(149, 287);
+            this.pbx8.Name = "pbx8";
+            this.pbx8.Size = new System.Drawing.Size(130, 130);
+            this.pbx8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx8.TabIndex = 0;
+            this.pbx8.TabStop = false;
+            this.pbx8.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // pbx9
+            // 
+            this.pbx9.Location = new System.Drawing.Point(281, 287);
+            this.pbx9.Name = "pbx9";
+            this.pbx9.Size = new System.Drawing.Size(130, 130);
+            this.pbx9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx9.TabIndex = 0;
+            this.pbx9.TabStop = false;
+            this.pbx9.Click += new System.EventHandler(this.SwitchPictureBox);
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnShuffle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnShuffle.ForeColor = System.Drawing.Color.White;
+            this.btnShuffle.Location = new System.Drawing.Point(532, 415);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(95, 42);
+            this.btnShuffle.TabIndex = 1;
+            this.btnShuffle.Text = "Aralashtirmoq";
+            this.btnShuffle.UseVisualStyleBackColor = false;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
+            // 
+            // lblMovesMade
+            // 
+            this.lblQadamlarSoni.AutoSize = true;
+            this.lblQadamlarSoni.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQadamlarSoni.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblQadamlarSoni.Location = new System.Drawing.Point(62, 454);
+            this.lblQadamlarSoni.Name = "lblMovesMade";
+            this.lblQadamlarSoni.Size = new System.Drawing.Size(125, 19);
+            this.lblQadamlarSoni.TabIndex = 2;
+            this.lblQadamlarSoni.Text = "Qadamlar soni:";
+            // 
+            // lblTimeElapsed
+            // 
+            this.lblQolganVaqt.AutoSize = true;
+            this.lblQolganVaqt.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQolganVaqt.Location = new System.Drawing.Point(578, 306);
+            this.lblQolganVaqt.Name = "lblTimeElapsed";
+            this.lblQolganVaqt.Size = new System.Drawing.Size(264, 80);
+            this.lblQolganVaqt.TabIndex = 3;
+            this.lblQolganVaqt.Text = "00:00:00";
+            // 
+            // tmrTimeElapse
+            // 
+            this.tmrTimeElapse.Enabled = true;
+            this.tmrTimeElapse.Interval = 900;
+            this.tmrTimeElapse.Tick += new System.EventHandler(this.YangilashQolganVaqt);
+            // 
+            // btnPause
+            // 
+            this.btnPauza.BackColor = System.Drawing.Color.Blue;
+            this.btnPauza.Enabled = false;
+            this.btnPauza.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPauza.ForeColor = System.Drawing.Color.White;
+            this.btnPauza.Location = new System.Drawing.Point(651, 415);
+            this.btnPauza.Name = "btnPause";
+            this.btnPauza.Size = new System.Drawing.Size(86, 42);
+            this.btnPauza.TabIndex = 1;
+            this.btnPauza.Text = "Pauza";
+            this.btnPauza.UseMnemonic = false;
+            this.btnPauza.UseVisualStyleBackColor = false;
+            this.btnPauza.Click += new System.EventHandler(this.PauzaYokiDavomEtish);
+            // 
+            // gbOriginal
+            // 
+            this.gbOriginal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gbOriginal.BackgroundImage")));
+            this.gbOriginal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gbOriginal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gbOriginal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.gbOriginal.Location = new System.Drawing.Point(535, 12);
+            this.gbOriginal.Name = "gbOriginal";
+            this.gbOriginal.Size = new System.Drawing.Size(315, 282);
+            this.gbOriginal.TabIndex = 0;
+            this.gbOriginal.TabStop = false;
+            this.gbOriginal.Text = "Asl Rasm";
+            // 
+            // frmPuzzleGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 495);
+            this.Controls.Add(this.btnPauza);
+            this.Controls.Add(this.lblQolganVaqt);
+            this.Controls.Add(this.lblQadamlarSoni);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.gbOriginal);
+            this.Controls.Add(this.gbPuzzleBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPuzzleGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Picture Puzzle-Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChiqishdanOldinRuxsatSora);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbPuzzleBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPuzzleBox;
+        private System.Windows.Forms.GroupBox gbOriginal;
+        private System.Windows.Forms.PictureBox pbx3;
+        private System.Windows.Forms.PictureBox pbx9;
+        private System.Windows.Forms.PictureBox pbx6;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.PictureBox pbx8;
+        private System.Windows.Forms.PictureBox pbx5;
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx7;
+        private System.Windows.Forms.PictureBox pbx4;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Label lblQadamlarSoni;
+        private System.Windows.Forms.Label lblQolganVaqt;
+        private System.Windows.Forms.Timer tmrTimeElapse;
+        public System.Windows.Forms.Button btnQuit;
+        public System.Windows.Forms.Button btnPauza;
+    }
+}
+
---
+++ puzzle/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmEkoPuzzle : Form
+    {
+        int inNullSliceIndex, qadamlar = 0;
+        List<Bitmap> lstOriginalRasmlarList = new List<Bitmap>(); // BitMap rasmlar listi olamiz
+        System.Diagnostics.Stopwatch taymer = new System.Diagnostics.Stopwatch(); // Taymer Obyekti aniqlanib olindi
+
+        public frmEkoPuzzle()
+        {
+            InitializeComponent(); // Komponentalar initsalizatsiya qilinsin
+            // 9 ta rasm bo\'lagi va bitta bo'sh rasm yuklanadi      
+            lstOriginalRasmlarList.AddRange(new Bitmap[] { Properties.Resources._1, Properties.Resources._2, Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7, Properties.Resources._8, Properties.Resources._9, Properties.Resources._null });
+            lblQadamlarSoni.Text += qadamlar;
+            lblQolganVaqt.Text = "00:00:00";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Shuffle();
+        }
+
+        void Shuffle()
+        {
+            do
+            {
+                int j;
+                List<int> Indexes = new List<int>(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 9 });// 8-indeks ko'rsatilmagan - u rasmning oxirgi bo'lagi
+                Random r = new Random();
+                for (int i = 0; i < 9; i++)
+                {
+                    Indexes.Remove((j = Indexes[r.Next(0, Indexes.Count)]));
+                    ((PictureBox)gbPuzzleBox.Controls[i]).Image = lstOriginalRasmlarList[j];
+                    if (j == 9) inNullSliceIndex = i;// Bo'sh (qora fon) rasm indeksini saqlab olamiz
+                }
+                // 2% holatda random terilgan rasm originali bilan bir xil bo'lib chiqadi
+            } while (GolibAniqlandimi()); // Agar Random terilgan puzzle original rasmni o'zi bo'lib chiqsa, jarayon takrorlansin
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            // Qaytadan boshlash Dialog oynasi
+            DialogResult HaYokiYoq = new DialogResult();
+            if (lblQolganVaqt.Text != "00:00:00")
+            {
+                HaYokiYoq = MessageBox.Show("QAYTA BOSHLAMOQCHIMSIZ ?", "Eko Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            if (HaYokiYoq == DialogResult.Yes || lblQolganVaqt.Text == "00:00:00")
+            {
+                Shuffle();
+                taymer.Reset();
+                lblQolganVaqt.Text = "00:00:00";
+                qadamlar = 0;
+                lblQadamlarSoni.Text = "Qadamlar soni : 0";
+            }
+        }
+
+        private void ChiqishdanOldinRuxsatSora(object sender, FormClosingEventArgs e)
+        {
+            DialogResult HaYokiYoq = MessageBox.Show("Haqiqatan Dasturdan ham chiqmoqchimisiz ?", "Eko Puzzle", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sender as Button != btnQuit && HaYokiYoq == DialogResult.No) e.Cancel = true; // Amalni bekor qilish
+            if (sender as Button == btnQuit && HaYokiYoq == DialogResult.Yes) Environment.Exit(0); // Dasturdan chiqib ketish
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            ChiqishdanOldinRuxsatSora(sender, e as FormClosingEventArgs);
+        }
+
+
+        /* <== TUIT & github.com/Zarifboyev 2024 ==>
+         * Bu dasturda rasmlar quyidagicha joylashgan, deb faraz qilamiz
+         *            * * *
+         *              * *
+         *            * * *   
+         * Ko'rib turganingizdek, bo'sh rasm 4-o'rinda joylashgan, uning atrofidagi RasmQutilardan,
+         * biri bosilganda undan tepada, pastda, chapdan va o'ngdan joylashgan RasmQutilarining Indekslarini olamiz
+         * Va agar shulardan birida bo'sh rasm joylashgan bo'lsa => if (YonQoshniRasmlar.Contains(bosh_rasm_indeks)
+         * Rasmlar o'rni almashtiriladi...
+         * */
+        private void SwitchPictureBox(object sender, EventArgs e)
+        {
+            if (lblQolganVaqt.Text == "00:00:00")
+                taymer.Start();
+            int inPictureBoxIndex = gbPuzzleBox.Controls.IndexOf(sender as Control);
+            if (inNullSliceIndex != inPictureBoxIndex)
+            {
+                // Belgilangan Rasm qutisining yon tomonlari, tepa va pastidagi indekslari hisob chiqiladi
+                List<int> YonQoshniRasmlar = new List<int>(new int[] { ((inPictureBoxIndex % 3 == 0) ? -1 : inPictureBoxIndex - 1), inPictureBoxIndex - 3, (inPictureBoxIndex % 3 == 2) ? -1 : inPictureBoxIndex + 1, inPictureBoxIndex + 3 });
+                // Agar Qora (bo'sh rasm) shu indekslardan birida tursa uning pozitsiyasi o'zgaritiriladi
+                if (YonQoshniRasmlar.Contains(inNullSliceIndex))
+                {
+                    // Bo'sh Rasm turgan qutining indeksiga Tanlanga Rasm qutisidagi rasm o'rnatiladi
+                    ((PictureBox)gbPuzzleBox.Controls[inNullSliceIndex]).Image = ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image;
+                    // Tanlanga rasmga esa o'z-o'zidan bo'sh (qora) rasm o'rnatiladi
+                    ((PictureBox)gbPuzzleBox.Controls[inPictureBoxIndex]).Image = lstOriginalRasmlarList[9];
+                    // Qora rasm turgan pozitsiyasi yangilanadi
+                    inNullSliceIndex = inPictureBoxIndex;
+                    // Qadamlar soni bittaga oshiriladi
+                    lblQadamlarSoni.Text = "Qadamlar Soni : " + (++qadamlar);
+                    if (GolibAniqlandimi())
+                    {
+                        taymer.Stop();
+                        (gbPuzzleBox.Controls[8] as PictureBox).Image = lstOriginalRasmlarList[8]; //Qora rasm o'rniga yashirilgan rasm bo'lagi joyiga qo'yib qo'yiladi
+                        MessageBox.Show("Tabriklaymiz🎉...\n Sizning Gulingiz Juda ham Xursand! 🎉 \n⌚ Vaqt ⌚: " + taymer.Elapsed.ToString().Remove(8) + "\nJami Qadamlar soni ⌚: " + qadamlar, "Eko Puzzle");
+                        qadamlar = 0;
+                        lblQadamlarSoni.Text = "Qadamlar soni : 0";
+                        lblQolganVaqt.Text = "00:00:00";
+                        taymer.Reset();
+                        Shuffle();
+                    }
+                }
+            }
+        }
+
+        bool GolibAniqlandimi()
+        {
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                if ((gbPuzzleBox.Controls[i] as PictureBox).Image != lstOriginalRasmlarList[i]) break;
+            }
+            if (i == 8) return true;
+            else return false;
+        }
+
+        private void YangilashQolganVaqt(object sender, EventArgs e)
+        {
+            if (taymer.Elapsed.ToString() != "00:00:00")
+                lblQolganVaqt.Text = taymer.Elapsed.ToString().Remove(8);
+            if (taymer.Elapsed.ToString() == "00:00:00")
+                btnPauza.Enabled = false;
+            else
+                btnPauza.Enabled = true;
+            if (taymer.Elapsed.Minutes.ToString() == "1")
+            {
+                taymer.Reset();
+                lblQadamlarSoni.Text = "Qadamlar soni : 0";
+                lblQolganVaqt.Text = "00:00:00";
+                qadamlar = 0;
+                btnPauza.Enabled = false;
+                MessageBox.Show("Vaqt tugadi\nQaytadan boshlash", "Eko Puzzle");
+                Shuffle();
+            }
+        }
+
+        private void PauzaYokiDavomEtish(object sender, EventArgs e)
+        {
+            if (btnPauza.Text == "Pauza")
+            {
+                taymer.Stop();
+                gbPuzzleBox.Visible = false;
+                btnPauza.Text = "Davom Etish";
+            }
+            else
+            {
+                taymer.Start();
+                gbPuzzleBox.Visible = true;
+                btnPauza.Text = "Pauza";
+            }
+        }
+    }
+}
